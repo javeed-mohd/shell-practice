2d3de91fef5c63d00b847ed1cf7a98fa3a2eca6c
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-02 23:36:53 +0530

    modified something

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This is the comment. This will not be executed by bash. This is used only for information purpose.
+#This is the comment. This will not be executed by bash. This is used only for the information purpose.
 echo "Hello World!"
 
-echo "Hi, I am learning GitHub..."
+echo "Hi, I am learning GitHub ..."
