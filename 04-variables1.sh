b229ecc18032b4f4187e2d7946529e7592a13072
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 18:25:18 +0530

    actions

--- 04-variables1.sh
+++ 04-variables1.sh
@@ -3,7 +3,7 @@
 PERSON1=$1 # No space i between value and =
 PERSON2=$2
 
-#$1, $2... are special characters which are used to get the output by entering the value in terminal
+#$1, $2... are special variables which are used to get the output by entering the value in terminal
 
 echo "$PERSON1:: Hello $PERSON2, How are you?" # If we insert $ at the beginning of the PERSON1, Shell assumes it as Variable, otherwise it will print as normal text
 echo "$PERSON2:: Hi $PERSON1, I am good. Thanks for asking. How are you?"
