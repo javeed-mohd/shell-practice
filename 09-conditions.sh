73253ae0a2583543c0f5559f6e85566e866aaae9
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 19:59:28 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -22,3 +22,13 @@ if [ $NUM -gt 21 ]; then
 else
     echo "Given number: $NUM is lesser than 21"
 fi
+
+# 3
+
+if [ $NUM -gt 21 ]; then
+    echo "Given number: $NUM is greater than 21"
+elif [ $NUM -eq 21 ]; then
+    echo "Given number: $NUM is equal to 21"
+else
+    echo "Given number: $NUM is lesser than 21"
+fi
