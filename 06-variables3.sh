1489aa979817a3530f3e029e48da3e80617c314e
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 18:21:11 +0530

    actions

--- 06-variables3.sh
+++ 06-variables3.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-TIMESTAMP=$(date) # date is a command whose output value is stored in timestamp varibale
+#   TIMESTAMP=$(date) # date is a command whose output value is stored in timestamp variable
 
-echo "Script executed at: $TIMESTAMP"
+#   echo "Script executed at: $TIMESTAMP"
+
+# New File
+
+START_TIME=$(date +%s)
+
+echo "Script executed at: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME)) # one braces is for mathematics and other is for command execution(output) to store in variable
+
+echo "Script executed in: $TOTAL_TIME"
