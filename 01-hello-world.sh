a2ec0a8077d222c0391b4a5122557ebf1354af9d
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 16:56:10 +0530

    modified

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#SHEBANG --> Shebang is the interpreter of the code in shell script that checks and compiles for errors
+
 #This is the comment. This will not be executed by bash. This is used only for the information purpose.
 echo "Hello World!"
 
---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am good. Thanks for asking. How are you?"
+echo "Ramesh:: I am doing great. What are you learning?"
+echo "Suresh:: I am learning Shell Scripting. What about you?" 
