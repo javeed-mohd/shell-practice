635a48bd6885af89127aa28a448e9a4e25404b19
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 19:30:31 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -7,9 +7,18 @@ SUM=$(($NUM1+$NUM2))
 
 echo " Sum is: $SUM"
 
+# New
+
 NUM1=500 # Everything in shell is string by default
-NUM2=Hello
+NUM2=Hello # It considers letter value as 0
 
 SUM=$(($NUM1+$NUM2))
 
 echo " Sum is: $SUM"
+
+
+# Array --> List of strings which are stored in continuous memory
+
+FRUITS=("Apple" "Banana" "Cherry")
+
+
