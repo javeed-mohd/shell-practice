fbcc0754dcc310eade222ecfd73bfbbad8a14ec0
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 17:20:40 +0530

    actions

---
+++ 04-variables1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 # No space i between value and =
+PERSON2=$2
+
+
+echo "$PERSON1:: Hello $PERSON2, How are you?" # If we insert $ at the beginning of the PERSON1, Shell assumes it as Variable, otherwise it will print as normal text
+echo "$PERSON2:: Hi $PERSON1, I am good. Thanks for asking. How are you?"
+echo "$PERSON1:: I am doing great. What are you learning?"
+echo "$PERSON2:: I am learning Shell Scripting. What about you?"
