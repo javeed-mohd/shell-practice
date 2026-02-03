c92a138057d396171573720f5db267405e368379
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 20:19:50 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
+    exit 1 # given exit 1 because we know exit code 0 executes
 fi
 
 echo "Installing nginx..."
