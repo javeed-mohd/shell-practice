75fe05aa3e3e0d73f4acd9920cffd8a9a91fff31
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 19:31:55 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,7 +5,7 @@ NUM2=1000
 
 SUM=$(($NUM1+$NUM2))
 
-echo " Sum is: $SUM"
+echo "Sum is: $SUM"
 
 # New
 
@@ -14,7 +14,7 @@ NUM2=Hello # It considers letter value as 0
 
 SUM=$(($NUM1+$NUM2))
 
-echo " Sum is: $SUM"
+echo "Sum is: $SUM"
 
 
 # Array --> List of strings which are stored in contiguous memory
