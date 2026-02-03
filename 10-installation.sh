eafaf23b60695aa9faf0fdf58143ba3d51d0d088
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-03 20:24:55 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,6 +7,12 @@ if [ $USERID -ne 0 ]; then
     exit 1 # given exit 1 because we know exit code 0 executes
 fi
 
-echo "Installing nginx..."
-
+echo "Installing Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx... FAILURE"
+    exit 1
+else 
+    echo "Installing Nginx... SUCCESS"
+fi
