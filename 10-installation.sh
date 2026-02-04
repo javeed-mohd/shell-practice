8a0238f7f8944aa23ff9e1e355c1a9f5f0f28043
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-04 14:42:29 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
-    exit 1 # given exit 1 because we know exit code 0 is for success
+    exit 1 # given exit 1 because it will not exit and we know this condition is error so we are giving exit code as other than 0. Because 0 is for success exit status
 fi
 
 echo "Installing Nginx"
