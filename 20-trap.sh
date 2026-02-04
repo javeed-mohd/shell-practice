1db8ad99fcf548837e5c90d7df1be179cb017f73
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-04 21:02:52 +0530

    actions

---
+++ 20-trap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e # This will be checking for errors, if errors it will Exit
+
+echo "Hello World!"
+echo "I am learning Bash Scripting"
+echoo "Printing error here"
+echo "Now, it is working fine"
