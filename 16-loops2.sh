63f9c270eebc749cbbf514c612d80ea692ee8319
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-04 20:15:00 +0530

    actions

---
+++ 16-loops2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# To log files
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"  # $0=12-logs.sh(script name)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER 
+
+# By default shell will not execute this, only it will be executed when called (It can be validate, status_check...)
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo "$2... FAILURE" | tee -a $LOGS_FILE # Tee is used for showing the output both in terminal and file (logs_file)
+        exit 1
+    else 
+        echo "$2... SUCCESS" | tee -a $LOGS_FILE
+    fi
+
+}
+
+for package in $@ # sudo sh 15-loops1.sh nginx mysql nodejs
+do
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package already installed, skipping"
+    fi
+done
