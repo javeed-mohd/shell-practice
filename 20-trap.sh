825a3b72dd54f6e527e79dbc4c52be2123dc3f23
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-04 21:13:09 +0530

    actions

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e # This will be checking for errors, if errors it will Exit
+set -e # ERR, This will be checking for errors, if errors it will Exit
+
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 echo "Hello World!"
 echo "I am learning Shell Scripting"
