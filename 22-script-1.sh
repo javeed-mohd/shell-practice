bd08cf72cdfb65fa4bc5facc794aa98aa23b9fe5
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-10 15:57:30 +0530

    actions

--- 22-script-1.sh
+++ 22-script-1.sh
@@ -5,4 +5,6 @@ COUNTRY=INDIA
 echo "I am in: $COUNTRY"
 echo "PID of SCRIPT-1: $$"
 
-sh 23-script-2.sh # It executes 23-script-2.sh after script-1
+# sh 23-script-2.sh # It executes 23-script-2.sh after script-1
+
+source 23-script-2.sh
