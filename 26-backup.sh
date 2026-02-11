eac9df92e3de68857bce4bc7cb5b15272d2b2864
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-11 16:45:32 +0530

    actions

--- 26-backup.sh
+++ 26-backup.sh
@@ -8,6 +8,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # Default value is 14 days, if the user does not supply any value
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" 
@@ -22,4 +25,14 @@ USAGE(){
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d SOURCE_DIR ]; then
+    echo "$R $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+if [ ! -d DEST_DIR ]; then
+    echo "$R $DEST_DIR does not exist $N"
+    exit 1
 fi
