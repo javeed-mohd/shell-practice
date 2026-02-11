2b27eef99a603056ed461dc01733da7e1ffe5fe0
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-11 20:58:49 +0530

    actions

--- 27-disk-usage.sh
+++ 27-disk-usage.sh
@@ -4,6 +4,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+MESSAGE=""
 
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
@@ -14,8 +15,8 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-    USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}' | cut -d "%" -f1)
-    PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}')
+    USAGE=$($line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$($line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE+="High Disk Usage on $PARTITION:$USAGE"
