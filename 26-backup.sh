6cc33bce0d71e940ce316d3ad3b20472d1286e88
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-11 18:28:33 +0530

    actions

--- 26-backup.sh
+++ 26-backup.sh
@@ -23,6 +23,10 @@ USAGE(){
     exit 1
 }
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
 if [ $# -lt 2 ]; then
     USAGE
 fi
@@ -36,3 +40,12 @@ if [ ! -d "$DEST_DIR" ]; then
     echo -e "$R Destination Directory: $DEST_DIR does not exist $N"
     exit 1
 fi
+
+### Finding the Files
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup Started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
