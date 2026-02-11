47c0dbaa4fb1cc51752996d4ea563073f8f52e92
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-11 15:56:47 +0530

    actions

--- 25-while-loop.sh
+++ 25-while-loop.sh
@@ -2,10 +2,15 @@
 
 count=1
 
-while [ $count -le 5 ]
-do
-  echo "Count is $count"
-  sleep 1
-  # Increment the counter
-  ((count++))
-done
+# while [ $count -le 5 ]
+# do
+#   echo "Count is $count"
+#   sleep 1
+#   # Increment the counter
+#   ((count++))
+# done
+
+while IFS= read -r LINE; do # IFS --> Internal Field Separation(Line by line separation)
+  # Process the line here
+  echo "Line content: $LINE"
+done < ./22-script-1.sh # Input which file to read
